73b8268cbf19831003303a72b296eed3767ed52f
Tom van Duist <tomvanduist@gmail.com>
2016-02-03 12:49:00 +0100

    QLSwift initial commit

---
+++ tomvanduist/QLSwift/QLSwift/Src/AppDelegate.swift
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.swift
+//  QLSwift
+//
+//  Created by Tom van Duist on 03/02/16.
+//
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    lazy var mainViewController: MainViewController =
+    {
+        return MainViewController(nibName:"MainView", bundle:nil)
+    }()
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool
+    {
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        
+        if let window = window {
+            window.rootViewController = mainViewController
+            window.makeKeyAndVisible()
+        }
+        
+        return true
+    }
+}
---
+++ tomvanduist/QLSwift/QLSwift/Src/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  QLSwift
+//
+//  Created by Tom van Duist on 03/02/16.
+//
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
