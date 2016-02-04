9a149e7f621d25316df65e790fa3fd8230e94052
Gorjan Jovanovski <jovanovski@gorjan.info>
2016-02-04 14:00:31 +0100

    error listener added

--- GorjanJovanovski/js/main.js
+++ GorjanJovanovski/js/main.js
@@ -6,12 +6,27 @@ var MyGrammerParser = require('MyGrammerParser');
 var MyGrammerListener = require('MyGrammerListener');
 var ast = {};
 
-function init(inputString){
+function initiate(inputString){
 	var characters = new antlr4.InputStream(inputString);
 	var lexer = new MyGrammerLexer.MyGrammerLexer(characters);
 	var tokens  = new antlr4.CommonTokenStream(lexer);
 	var parser = new MyGrammerParser.MyGrammerParser(tokens);
+	
+	var ErrorListener = function() {
+		antlr4.error.ErrorListener.call(this);
+	  	return this;
+	};
+
+	ErrorListener.prototype = Object.create(antlr4.error.ErrorListener.prototype);
+	ErrorListener.prototype.constructor = ErrorListener;
+	ErrorListener.prototype.syntaxError = function(rec, sym, line, col, msg, e) {
+	  	console.log("OPA: " + line + ' '+ msg);
+	};
+
+
+
 	parser.buildParseTrees = true;
+	parser.addErrorListener(new ErrorListener());
 	var tree = parser.form();
 	parseQuestions(tree);
 }	
@@ -20,8 +35,6 @@ function parseQuestions(parseTree){
 	
 	var questions = new Array();
 	var dependencies = new Array();
-
-	var questionID = 0;
 
 	var QuestionPrinter = function () {
     	MyGrammerListener.MyGrammerListener.call(this);
@@ -46,11 +59,24 @@ function parseQuestions(parseTree){
 	    var question = {};
 
 	    question.text = questionNode.children[0].getText();
+	    question.text = question.text.substring(1, question.text.length-1);
+
 	    question.label = questionNode.children[1].getText();
 
 	    if(questionNode.children[3].children.length==1){
 	    	//just a terminal node
 	    	question.type = questionNode.children[3].getText();
+
+		    if(question.type=="number" || question.type=="float" || question.type=="money"){
+		    	question.value = 0;
+		    }
+		    else if(question.type=="boolean"){
+		    	question.value = false;
+		    }
+		    else if(question.type=="string"){
+		    	question.value = "";
+		    }
+
 	    }
 	    else{
 	    	//complex expr node
@@ -63,15 +89,7 @@ function parseQuestions(parseTree){
 	    	question.dependencies = dependencies.slice(0);
 	    }
 
-	    if(question.type=="number" || question.type=="float" || question.type=="money"){
-	    	question.value = 0;
-	    }
-	    else if(question.type=="boolean"){
-	    	question.value = false;
-	    }
-	    else if(question.type=="string"){
-	    	question.value = "";
-	    }
+	    question.visible = true;
 
 	    questions.push(question);
 	};
@@ -85,19 +103,6 @@ function parseQuestions(parseTree){
 
 	var printer = new QuestionPrinter();
 	antlr4.tree.ParseTreeWalker.DEFAULT.walk(printer, parseTree);
-}
-
-function setHandlers(){
-	$("input").change(function(){
-		var label = $(this).attr("name");
-		if($(this).attr("type")=="checkbox"){
-			setASTQuestionValue(label, $(this).is(":checked"));
-		}
-		else{
-			setASTQuestionValue(label, $(this).val());
-		}
-		refreshGUI();
-	});
 }
 
 function createAST(questions){
@@ -125,6 +130,21 @@ function setASTQuestionValue(label, value){
 	}
 }
 
+
+function setHandlers(){
+	$("input").change(function(){
+		var label = $(this).attr("name");
+		if($(this).attr("type")=="checkbox"){
+			setASTQuestionValue(label, $(this).is(":checked"));
+		}
+		else{
+			setASTQuestionValue(label, $(this).val());
+		}
+		refreshGUI();
+	});
+}
+
+
 function refreshGUI(){
 	for(var i=0;i<ast.questions.length;i++){
 		var question = ast.questions[i];
@@ -141,10 +161,12 @@ function refreshGUI(){
 
 			if(shouldShow){
 				$("div[label='"+question.label+"']").show();
+				ast.questions[i].visible = true;
 				
 			}
 			else{
 				$("div[label='"+question.label+"']").hide();
+				ast.questions[i].visible = false;
 			}
 		}
 		if(question.expr!=undefined){
@@ -187,36 +209,33 @@ function getQuestionType(label){
 }
 
 function generateQuestionHTML(question){
-	var html = "<div label='" + question.label + "'> ["
-		+ question.label + " - " + question.type + "] " + question.text + " ";
+	var html = "<div label='" + question.label + "'>";
+	html += "<label class='question'>" + question.text + " ";
+    html += "<input name='"+ question.label + "' type='";
+
 	switch(question.type){
-		case "integer": html += "<input name='"+ question.label
-			+ "' type='number'";
-						break;
-		case "money": html += "<input name='"+ question.label
-			+ "' type='number'";
-						break;
-		case "float": html += "<input name='"+ question.label
-			+ "' type='number'";
-						break;
-		case "string": html += "<input name='"+ question.label
-			+ "' type='text'";
-						break;
-		case "boolean": html += "<input name='"+ question.label
-			+ "' type='checkbox'";
-						break;
-		default: html += "<input name='"+ question.label
-			+ "' type='text'";
-						break;
+		case "integer": 	html += "number";
+							break;
+		case "money": 		html += "number";
+							break;
+		case "float": 		html += "number";
+							break;
+		case "string": 		html += "text";
+							break;
+		case "boolean": 	html += "checkbox";
+							break;
+		default: 			html += "text";
+							break;
 	}
+
+	html += "'";
 
 	if(question.expr != undefined){
 		html += " disabled";
 	}
 
-	html += "/>";
-	
-	html += "</div>";
+	html += "/></label></div>";
+
 	return html;
 }
 
@@ -231,6 +250,26 @@ function renderQuestions(questions){
 }
 
 $("#generate").click(function(){
-	var input = $("#input").val().replace(/[\r\t]/g, "").trim();
-	init(input);
+	initiate($("#input").val());
 });
+
+$("#save").click(function(){
+	var answers = new Array();
+	for(var i=0;i<ast.questions.length;i++){
+		var question = ast.questions[i];
+		if(question.visible){
+			var answer = {};
+			answer.questionLabel = question.label;
+			answer.questionText = question.text;
+			answer.questionType = question.type;
+			answer.value = question.value;
+			answers.push(answer);
+		}
+	}
+	var blob = new Blob([JSON.stringify(answers, null, 2)], {type: "text/plain;charset=utf-8"});
+	saveAs(blob, "answers.txt");
+	
+});
+
+//Start
+initiate($("#input").val());
