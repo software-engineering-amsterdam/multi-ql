ab4f85aa1099612db5b573ea69490b9ee260881e
Felix Barten <felixbarten@gmail.com>
2016-02-04 13:20:48 +0100

    added lex/yacc to build of project, project builds now

--- FelixBarten/QL/QL/Program.fs
+++ FelixBarten/QL/QL/Program.fs
@@ -1,8 +1,28 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
+open System   
+open Sql   
+ 
+let x = "   
+    SELECT x, y, z   
+    FROM t1   
+    LEFT JOIN t2   
+    INNER JOIN t3 ON t3.ID = t2.ID   
+    WHERE x = 50 AND y = 20   
+    ORDER BY x ASC, y DESC, z   
+"   
+ 
+
 
 [<EntryPoint>]
 let main argv = 
     printfn "%A" argv
+
+    let lexbuf = Microsoft.FSharp.Text.Lexing.LexBuffer<_>.FromString x
+    let y = Parser.start Lexer.tokenize lexbuf   
+    printfn "%A" y   
+ 
+    Console.WriteLine("(press any key)")   
+    Console.ReadKey(true) |> ignore
     0 // return an integer exit code
