099e18b8efadd09125cbbde4a34b045f2c5adeb0
Olav Trauschke <olav.trauschke@student.uva.nl>
2016-02-10 11:50:29 +0100

    Further testing, bug disappeared

--- olavTrauschke/QL/test/org/uva/sea/ql/parser/ParserTest.java
+++ olavTrauschke/QL/test/org/uva/sea/ql/parser/ParserTest.java
@@ -9,14 +9,31 @@ import org.uva.sea.ql.ast.expr.*;
 public class ParserTest {
 
     @Test
-    public void testExpressionParsing() throws FileNotFoundException {
-        Lexer lexer = new Lexer("expressionTest.ql");
+    public void testSimpleExpressionParsing() throws FileNotFoundException {
+        Lexer lexer = new Lexer("simpleExpression.ql");
         lexer.nextToken();
         Parser parser = new Parser(lexer);
         boolean parsed = parser.parse();
         assertTrue(parsed);
         ASTNode result = parser.getResult();
         ASTNode expected = new Add(new Int(1), new Int(1));
+        assertEquals(expected, result);
+    }
+    
+    public void testComplexExpressionParsing() throws FileNotFoundException {
+        Lexer lexer = new Lexer("complexExpression.ql");
+        lexer.nextToken();
+        Parser parser = new Parser(lexer);
+        boolean parsed = parser.parse();
+        assertTrue(parsed);
+        ASTNode result = parser.getResult;
+        ASTNode firstTerm = new Ident("a");
+        ASTNode firstFactor = new Int(2);
+        ASTNode secondFactor = new Ident("c");
+        ASTNode secondTerm = new Mul(firstFactor, secondFactor);
+        ASTNode firstTerms = new Add(firstTerm, secondTerm);
+        ASTNode lastTerm = new Ident("d");
+        ASTNode expected = new Add(firstTerms, lastTerm);
         assertEquals(expected, result);
     }
     
