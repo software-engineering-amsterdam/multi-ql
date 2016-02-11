0f55046560accc813044419fbf370ff1f404eb2f
James Bartholomew <anxys@hotmail.co.uk>
2016-02-11 09:15:31 +0100

    Started working on type checking

--- anxys/QL/src/Lib.hs
+++ anxys/QL/src/Lib.hs
@@ -1,15 +1,14 @@
 module Lib
-    ( someFunc
-      , run
-    ) where
+     where
 
--- TODO move everyting to Parsing
+-- TODO move everything to Parsing
 
-import Text.ParserCombinators.Parsec as P
-import Text.ParserCombinators.Parsec.Language
-import qualified Text.ParserCombinators.Parsec.Token as Token
-import Text.Parsec.Expr
-import Ast
+import           Ast                                    as A
+import           Text.Parsec.Prim                       as S
+import           Text.ParserCombinators.Parsec          as P
+import           Text.ParserCombinators.Parsec.Expr
+import           Text.ParserCombinators.Parsec.Language
+import qualified Text.ParserCombinators.Parsec.Token    as Token
 
 languageDef =
             emptyDef { Token.commentStart    = "/*"
@@ -29,113 +28,149 @@ languageDef =
                                                , "integer"
                                                , "string"
                                                ]
-                     , Token.reservedOpNames = ["+", "-", "*", "/", "and", "or", "not", ":"]
+                     , Token.reservedOpNames = ["+"
+                                               , "-"
+                                               , "*"
+                                               , "/"
+                                               , "and"
+                                               , "or"
+                                               , "not"
+                                               , ":"
+                                               , "++"
+                                               , ">"
+                                               , "<"
+                                               , "=="
+                                               , "!="
+                                               , ">="
+                                               , "<="]
                      , Token.caseSensitive = True
                    }
 
 lexer = Token.makeTokenParser languageDef
-
 identifier = Token.identifier lexer
-reserved   = Token.reserved   lexer
+reserved = Token.reserved   lexer
 reservedOp = Token.reservedOp lexer
-parens     = Token.parens     lexer
-braces     = Token.braces     lexer
-stringLiteral = Token.stringLiteral lexer
-integer    = Token.integer    lexer
+parens = Token.parens     lexer
+braces = Token.braces     lexer
+integer = Token.integer    lexer
 whiteSpace = Token.whiteSpace lexer
+float = Token.float lexer
+stringLiteral = Token.stringLiteral lexer
 
-stringL :: Parser String
-stringL = do
-         char '"'
-         v <- many anyChar 
-         char '"'
-         return v
-
-money :: Parser FieldType
-money = do
+moneyT :: Parser FieldType
+moneyT = do
       reserved "money"
-      return Money
+      return Money <?> "money type"
 
 integerT :: Parser FieldType
 integerT = do
          reserved "integer"
-         return Integer
+         return Integer <?> "integer type"
+
+boolT :: Parser FieldType
+boolT =  do
+         reserved "bool"
+         return Boolean <?> "bool type"
 
 stringT  :: Parser FieldType
 stringT = do
-        reserved "string"   
-        return String
+        reserved "string"
+        return String <?> "string type"
 
 fieldT :: Parser FieldType
-fieldT = money <|> integerT <|> stringT
+fieldT = moneyT <|> integerT <|> stringT <|> boolT <?> "field type"
 
 field :: Parser Stmnt
-field = do
-      label <- stringLiteral
+--field = Field <$> (P.try calculatedField <|> simpleField)
+field =   do
+  loc <- getPosition
+  a <- P.try calculatedField <|> simpleField
+  return ( Field a, loc)
+
+simpleField :: Parser Field
+simpleField = do
+      text <- stringLiteral
       name <- identifier
       reserved ":"
       t <-fieldT
-      return (Field label name t "None")
+      return (SimpleField text name t) <?> "field"
 
-defaultField :: Parser Stmnt
-defaultField = return (Field "DisplayText" "IdTest" Money "ValuesTest")
+calculatedField :: Parser Field
+calculatedField = do
+      text <- stringLiteral
+      name <- identifier
+      reserved ":"
+      t <-fieldT
+      reserved "="
+      e <- expr
+      return (CalculatedField text name t e ) <?> "field"
 
 block :: Parser Block
-block =  do
-      body  <- braces $ many stmnt
-      return body
+block = braces (many stmnt) <?> "block"
 
-expr :: Parser Expr  --Todo
-expr = do
-     v <- lit
-     return v
+expr :: Parser Expr
+expr = buildExpressionParser table term <?> "expression"
+
+table :: [[Operator Char a Expr]]
+table = [ [unary "not" (UnOp Neg)]
+        , [binary "and" (BinOp And) AssocLeft, binary "or" (BinOp Or) AssocLeft]
+        , [binary "==" (BinOp A.EQ) AssocLeft, binary "!=" (BinOp A.NEQ) AssocLeft]
+        , [binary ">" (BinOp A.GT) AssocLeft, binary "<" (BinOp A.LT) AssocLeft]
+        , [binary ">=" (BinOp A.GTE) AssocLeft, binary "<=" (BinOp A.LTE) AssocLeft]
+        , [binary "*" (BinOp Mul) AssocLeft, binary "/" (BinOp Div) AssocLeft ]
+        , [binary "+" (BinOp Add) AssocLeft, binary "-" (BinOp Sub) AssocLeft ]
+        , [binary "++" (BinOp SConcat) AssocLeft]
+        ]
+  where binary name f = Infix ( reservedOp name >> return f)
+        unary name f = Prefix ( reservedOp name >> return f)
+
+term :: Parser Expr
+term = parens expr
+       <|> fmap Var identifier
+       <|> lit
+       <?> "term"
 
 lit :: Parser Expr
-lit = do
-    r <- slit <|> ilit <|> mlit <|> blit
-    return (Lit r)
+lit = Lit <$> (slit <|> P.try mlit <|> ilit <|> blit) <?> "literal"
 
 slit :: Parser Lit
-slit = do
-     v <- stringLiteral
-     return (SLit v)
+slit = SLit <$> stringLiteral <?> "string literal"
 
-mlit :: Parser Lit -- TODO
-mlit = do
-     val <- many1 digit
-     return (ILit $ read val)
+mlit :: Parser Lit
+mlit = MLit <$> float <?> "money literal"
 
 ilit :: Parser Lit
-ilit = do
-     val <- many1 digit
-     return (ILit $ read val)
+ilit = ILit <$> integer <?> "integer literal"
 
 blit :: Parser Lit
-blit = do
-     r <- (true <|> false)
-     return (BLit  r )
-
-true :: Parser Bool
-true = do
-     reserved "true"
-     return True 
-
-false :: Parser Bool
-false = do 
-      reserved "false"
-      return False
+blit = BLit <$>  (true <|> false) <?> "bool literal"
+     where
+     true = reserved "true" >> return True
+     false = reserved "false" >> return False
 
 stmnt :: Parser Stmnt
-stmnt = do
-      r <- ifStmnt <|> field
-      return r
+stmnt = conditional <|> field <?> "statement"
+
+conditional :: Parser Stmnt
+conditional = P.try ifElseStmnt <|> ifStmnt <?> "conditional"
 
 ifStmnt :: Parser Stmnt
 ifStmnt = do
+        loc <- getPosition
         reserved "if"
         cond <- parens expr
         body <- block
-        return (If cond body)
+        return ((If cond body), loc) <?> "if statement"
+
+ifElseStmnt :: Parser Stmnt
+ifElseStmnt = do
+        loc <- getPosition
+        reserved "if"
+        cond <- parens expr
+        firstBody <- block
+        reserved "else"
+        secondBody <- block
+        return ((IfElse cond firstBody secondBody), loc) <?> "if else statement"
 
 form :: Parser Form
 form = do
@@ -148,16 +183,15 @@ parse :: String
 parse = undefined
 
 -- When values are changed check all variables in the environment to make sure if their value must be recomputed and then rerender the UI
--- E.G: 
---lookup :: FiniteMap -> id  -> Maybe Value 
+-- E.G:
+--lookup :: FiniteMap -> id  -> Maybe Value
 -- eval :: Expr -> a
 -- eval AExpr
 
-run :: String -> String 
+run :: String -> String
 run input = case P.parse form "ql" input of
           Left err ->  "Error: " ++ show err
           Right val -> show val
 
-someFunc :: String ->  String
-someFunc  = run
+
 
--- anxys/QL/test/Spec.hs
+++ anxys/QL/test/Spec.hs
@@ -1,31 +1,98 @@
-import Test.Hspec
-import Lib
-import Ast as A
-
-simpleForm :: String
-simpleForm = "form name { }"
-
-ifForm :: String
-ifForm = "form name { if (false) {} }"
-
-ifFormWithField :: String
-ifFormWithField = "form name { if (false) {\"Display Text One\" idTest1: money} }"
-
-testData :: String
-testData = "form taxOfficeExample { \n \"Display Text One\"\n \t idTest1: money \n \"DisplayText2\"\n \t idTest2: integer \n}"
+import           Ast        as A
+import   TypeChecker
+import           Lib
+import           Test.Hspec
 
 main :: IO ()
 main = hspec $
   describe "Lib. parse" $ do
+  describe "Parsing Forms" $ do
 
     it "can parse an empty form" $
-      someFunc simpleForm `shouldBe` show (Form "name" [])
+      tParse form "form name { }" `shouldBe` show (Form "name" [])
 
     it "can parse a Form with two fields " $
-      someFunc testData `shouldBe` show (Form "taxOfficeExample" [Field {A.label = "Display Text One", A.id = "idTest1", fieldType = Money, value = "None"},Field {A.label = "DisplayText2", A.id = "idTest2", fieldType = Integer, value = "None"}])
+      tParse form "form taxOfficeExample { \"Display Text One\" idTest1: money \"DisplayText2\" \t idTest2: integer}" `shouldBe` show (Form "taxOfficeExample" [Field SimpleField {A.label = "Display Text One", A.id = "idTest1", fieldType = Money}, Field SimpleField {A.label = "DisplayText2", A.id = "idTest2", fieldType = Integer}])
 
     it "can parse a Form with an if statement" $
-      someFunc ifForm `shouldBe` show (Form "name" [If (Lit (BLit False)) []])
+      tParse form "form name { if (false) {} }" `shouldBe` show (Form "name" [If (Lit (BLit False)) []])
 
     it "can parse a Form with an if statement containing fields" $
-      someFunc ifFormWithField `shouldBe` show (Form "name" [If (Lit (BLit False)) [Field {A.label = "Display Text One", A.id = "idTest1", fieldType = Money, value = "None"}]])
+      tParse form "form name { if (false) {\"Display Text One\" idTest1: money} }" `shouldBe` show (Form "name" [If (Lit (BLit False)) [Field  SimpleField {A.label = "Display Text One", A.id = "idTest1", fieldType = Money }]])
+
+
+  describe "Parsing Expressions" $ do
+    it "can parse an addition expression" $
+      tParse expr "1 + 2" `shouldBe` show (BinOp Add (Lit $ ILit 1) (Lit $ ILit 2))
+
+    it "can parse a GT comparison" $
+      tParse expr "1 > 2" `shouldBe` show (BinOp A.GT (Lit $ ILit 1) (Lit $ ILit 2))
+
+    it "can parse a LT comparison" $
+      tParse expr "1 < 2" `shouldBe` show (BinOp A.LT (Lit $ ILit 1) (Lit $ ILit 2))
+
+    it "can parse an and expression" $
+      tParse expr "true and false" `shouldBe` show (BinOp And (Lit $ BLit True) (Lit $ BLit False))
+
+    it "can parse an or expression" $
+      tParse expr "true or false" `shouldBe` show (BinOp Or (Lit $ BLit True) (Lit $ BLit False))
+
+    it "can parse a literal" $
+      tParse expr "1" `shouldBe` show (Lit $ ILit 1)
+
+    it "can parse a var addition" $
+      tParse expr "testValue + 2" `shouldBe` show (BinOp Add (Var "testValue") (Lit $ ILit 2))
+
+    it "can parse string literal" $
+      tParse expr "\"TestString\"" `shouldBe` show (Lit $ SLit "TestString")
+
+    it "can parse money literal" $
+      tParse expr "12.22" `shouldBe` show (Lit $ MLit 12.22)
+
+
+  describe "Parsing control flow" $ do
+    it "can parse an if statement" $
+      tParse ifStmnt "if(false) {}" `shouldBe` show (If (Lit $ BLit False) [])
+
+    it "can parse an if else statement" $
+      tParse ifElseStmnt "if(true) {} else {}" `shouldBe` show (IfElse (Lit $ BLit True) [] [])
+
+    it "can parse control flow parsing" $
+      tParse conditional "if(true) {} else {}" `shouldBe` show (IfElse (Lit $ BLit True) [] [])
+
+    it "can parse control flow parsing" $
+      tParse conditional "if(true) {}" `shouldBe` show (If (Lit $ BLit True) [])
+
+
+  describe "Parsing fields" $ do
+    it "can parse a simple field" $
+      tParse simpleField "\"Display Text One\" idTest1: money" `shouldBe` show SimpleField {A.label = "Display Text One", A.id = "idTest1", fieldType = Money}
+
+    it "can parse a calculated field" $
+      tParse calculatedField "\"Display Text One\" idTest1: money = 10.00" `shouldBe` show CalculatedField {A.label = "Display Text One", A.id = "idTest1", fieldType = Money, A.exp = Lit $ MLit 10.00}
+
+    it "can parse a calculated field with a boolean condition" $
+      tParse calculatedField "\"Display Text One\" idTest1: bool = true or true" `shouldBe` show CalculatedField {A.label = "Display Text One", A.id = "idTest1", fieldType = Boolean, A.exp = BinOp Or (Lit $ BLit True) (Lit $ BLit True)}
+
+    it "can parse a calculated field with a money addition" $
+      tParse calculatedField "\"Display Text One\" idTest1: money = 11.00 + 12.00" `shouldBe` show CalculatedField {A.label = "Display Text One", A.id = "idTest1", fieldType = Money, A.exp = BinOp Add (Lit $ MLit 11.00) (Lit $ MLit 12.00)}
+
+    it "can parse a calculated field with a money addition including a variable identifier" $
+      tParse calculatedField "\"Display Text One\" idTest1: money = testVar + 12.00" `shouldBe` show CalculatedField {A.label = "Display Text One", A.id = "idTest1", fieldType = Money, A.exp = BinOp Add (Var "testVar") (Lit $ MLit 12.00)}
+
+
+  describe "TypeChecker" $ do
+    it "can type determine the type of this expression (Integer)" $
+     getType [] (BinOp Add (Lit (ILit 12)) (Lit (ILit 12))) `shouldBe` Just Integer
+
+    it "can type determine the type of this expression (Boolean)" $
+     getType [] (BinOp And (Lit (BLit True)) (Lit (BLit True))) `shouldBe` Just Boolean
+
+    it "can type determine the type of this expression (Boolean)" $
+     getType [] (BinOp Add (Lit (MLit 22.00)) (Lit (MLit 22.00))) `shouldBe` Just Money
+
+    it "can not type determine the type of this expression (Money + Integer)" $
+     getType [] (BinOp Add (Lit (MLit 22.00)) (Lit (ILit 1))) `shouldBe` Nothing 
+
+    it "can not type determine the type of this expression (Money + Boolean)" $
+     getType [] (BinOp Add (Lit (MLit 22.00)) (Lit (BLit False))) `shouldBe` Nothing 
