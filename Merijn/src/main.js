32dc09428f4e5463e1354d8e3152417de57a8112
Merijn Wijngaard <mwijngaard@gmail.com>
2016-02-11 13:04:17 +0100

    implemented test app ui

--- Merijn/src/main.js
+++ Merijn/src/main.js
@@ -1,34 +1,36 @@
-import { QlParser } from 'src/parser';
+import { AnalyzingQlParser } from 'src/parser';
 import { NodeNormalizer } from 'src/ast_normalization';
 import { SemanticAnalyser } from 'src/ast_semantic_analysis';
 import * as ace from 'ace';
 
-var LOCALSTORAGE_KEY = 'uva-software-process-ql-merijn-last-input';
-
-let editor = ace.edit('input'),
+let LOCALSTORAGE_KEY = 'uva-software-process-ql-merijn-last-input',
+	editor = ace.edit('input'),
 	session = editor.getSession(),
 	logElement = document.getElementById('log'),
-	renderElement = document.getElementById('render');
-
-var parser = new QlParser(new SemanticAnalyser());
+	renderElement = document.getElementById('render'),
+	parser = new AnalyzingQlParser(new SemanticAnalyser());
 
 session.on('change', function (e) {
-	window.localStorage.setItem(LOCALSTORAGE_KEY, session.getValue());
+	let val = session.getValue(),
+		parseResult = parser.parse(val),
+		errors = parseResult.errors;
+
+	session.clearAnnotations();
+	if (errors.length > 0) {
+		logElement.value = errors.map((lineError) => {
+			return "[line " + lineError.line + "] " + lineError.message;
+		}).join("\n");
+		session.setAnnotations(errors.map((lineError) => {
+			return {
+				'row': lineError.line-1,
+				'text': lineError.message,
+				'type': 'error'
+			};
+		}));
+	} else {
+		logElement.value = "Ok";
+	}
+
+	window.localStorage.setItem(LOCALSTORAGE_KEY, val);
 });
 session.setValue(window.localStorage.getItem(LOCALSTORAGE_KEY));
-
-//var parser = new QlParser(new SemanticAnalyser());
-//let ast = parser.parse(`
-//	Form {
-//		"foo" bar boolean;
-//		"bar" baz string;
-//		"baz" quux integer;
-//		"quux" foo float;
-//		"quux" foo money;
-//		if (bar == false) {
-//			"foo" bar integer;
-//		} else {
-//			"bar" baz integer;
-//		}
-//	}
-//`);
