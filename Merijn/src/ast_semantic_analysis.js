6eba3077962dab4e86f89b356aea3d26f441fc06
Merijn Wijngaard <mwijngaard@gmail.com>
2016-02-11 14:45:14 +0100

    moved scope to separate file, to be reused during rendering

--- Merijn/src/ast_semantic_analysis.js
+++ Merijn/src/ast_semantic_analysis.js
@@ -1,35 +1,9 @@
 import {NodeListener, NodeWalker} from 'src/ast_walking';
 import * as ast from 'src/ast';
 import { LineError } from 'src/error';
+import { Scope } from 'src/scope';
 
 let TYPE_UNKNOWN = 'unknown';       // used when types cannot be resolved - ie. for undefined identifiers
-
-class Scope {
-	constructor(parent_scope) {
-		this.parent_scope = parent_scope || null;
-		this.scope = {};
-	}
-	has (name) {
-		return name in this.scope || this.parent_scope !== null && this.parent_scope.has(name);
-	}
-	typeOf (name) {
-		if (name in this.scope) {
-			return this.scope[name];
-		}
-
-		if (this.parent_scope !== null) {
-			return this.parent_scope.typeOf(name);
-		}
-
-		throw new Error("No name `" + name + "` in scope");
-	}
-	set (name, type) {
-		if (this.has(name)) {
-			throw new Error("Name `" + name + "` already defined");
-		}
-		this.scope[name] = type;
-	}
-}
 
 class NodeErrorLog {
 	constructor() {
@@ -192,7 +166,7 @@ class SementicAnalysisVisitor extends ast.NodeVisitor {
 			return TYPE_UNKNOWN;
 		}
 
-		return scope.typeOf(name);
+		return scope.get(name);
 	}
 }
 
