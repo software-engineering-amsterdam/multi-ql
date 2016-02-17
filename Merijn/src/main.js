8a0a4ac16d1b7fb488c5219bcaa2990af56b4cff
Merijn Wijngaard <mwijngaard@gmail.com>
2016-02-17 16:19:08 +0100

    implemented rendering using observables

--- Merijn/src/main.js
+++ Merijn/src/main.js
@@ -1,6 +1,9 @@
 import { AnalyzingQlParser } from 'src/parser';
 import { NodeNormalizer } from 'src/ast_normalization';
 import { SemanticAnalyser } from 'src/ast_semantic_analysis';
+import { WidgetFactory } from 'src/widgets';
+import { ExprObservableFactory } from 'src/expr_observable';
+import { Renderer } from 'src/rendering';
 import * as ace from 'ace';
 
 let LOCALSTORAGE_KEY = 'uva-software-process-ql-merijn-last-input',
@@ -8,7 +11,8 @@ let LOCALSTORAGE_KEY = 'uva-software-process-ql-merijn-last-input',
 	session = editor.getSession(),
 	logElement = document.getElementById('log'),
 	renderElement = document.getElementById('render'),
-	parser = new AnalyzingQlParser(new SemanticAnalyser());
+	parser = new AnalyzingQlParser(new SemanticAnalyser()),
+	renderer = new Renderer(document, new WidgetFactory(document), new ExprObservableFactory());
 
 session.on('change', function (e) {
 	let val = session.getValue(),
@@ -29,6 +33,10 @@ session.on('change', function (e) {
 		}));
 	} else {
 		logElement.value = "Ok";
+		while (renderElement.firstChild !== null) {     // TODO make this reusable
+			renderElement.removeChild(renderElement.firstChild);
+		}
+		renderer.render(parseResult.ast, renderElement);
 	}
 
 	window.localStorage.setItem(LOCALSTORAGE_KEY, val);
---
+++ Merijn/src/rendering.js
@@ -0,0 +1,132 @@
+import * as ast from 'src/ast';
+import { Scope } from 'src/scope';
+import { ExprObservableFactory } from 'src/expr_observable';
+
+class IfUpdater {
+	constructor(ifContainerElement, exprObservable, blockNode, renderingVisitor, scope) {
+		this.ifContainerElement = ifContainerElement;
+		this.exprObservable = exprObservable;
+		this.blockNode = blockNode;
+		this.renderingVisitor = renderingVisitor;
+		this.scope = scope;
+	}
+	notify() {
+		while (this.ifContainerElement.firstChild !== null) {
+			this.ifContainerElement.removeChild(this.ifContainerElement.firstChild);
+		}
+		if (this.exprObservable.getValue() === true) {
+			this.blockNode.accept(this.renderingVisitor, this.ifContainerElement, this.scope);
+		}
+	}
+}
+
+class QuestionRenderer {
+	constructor(elementFactory, widgetFactory) {
+		this.elementFactory = elementFactory;
+		this.widgetFactory = widgetFactory;
+	}
+	render(questionNode, containerElement, scope) {
+		let questionContainerElement = this.elementFactory.createElement('div'),
+			questionlabelElement = this.elementFactory.createElement('label'),
+			widget;
+
+		questionContainerElement.setAttribute('class', 'question');
+		questionlabelElement.textContent = questionNode.description;
+		questionContainerElement.appendChild(questionlabelElement);
+
+		widget = this.renderWidget(questionNode, questionContainerElement);
+
+		scope.set(questionNode.name, widget);
+		containerElement.appendChild(questionContainerElement);
+	}
+	/** @protected */
+	renderWidget() {
+		throw new Error("Override in subclasses");
+	}
+}
+
+class InputQuestionRenderer extends QuestionRenderer {
+	constructor(elementFactory, widgetFactory) {
+		super(elementFactory, widgetFactory);
+	}
+	renderWidget(inputQuestionNode, questionContainerElement) {
+		return this.widgetFactory.renderWidget(inputQuestionNode.type, questionContainerElement, false);
+	}
+}
+
+class ExprQuestionUpdater {
+	constructor(widget, exprObservable) {
+		this.widget = widget;
+		this.exprObservable = exprObservable;
+	}
+	notify() {
+		this.widget.setValue(this.exprObservable.getValue());
+	}
+}
+
+class ExprQuestionRenderer extends QuestionRenderer {
+	constructor(elementFactory, widgetFactory, exprObservableFactory) {
+		super(elementFactory, widgetFactory);
+		this.exprObservableFactory = exprObservableFactory;
+	}
+	renderWidget(exprQuestionNode, questionContainerElement, scope) {
+		let exprObservable = this.exprObservableFactory.createExprObservable(exprQuestionNode.expr, scope),
+			widget = this.widgetFactory.renderWidget(exprObservable.type, questionContainerElement, true),
+			updater = new ExprQuestionUpdater(widget, exprObservable);
+
+		exprObservable.registerObserver(updater);
+		updater.notify();
+		return widget;
+	}
+}
+
+class RenderingVisitor extends ast.NodeVisitor {
+	constructor(elementFactory, widgetFactory, exprObservableFactory) {
+		super();
+		this.elementFactory = elementFactory;
+		this.widgetFactory = widgetFactory;
+		this.exprObservableFactory = exprObservableFactory;
+		this.inputQuestionRenderer = new InputQuestionRenderer(elementFactory, widgetFactory);
+		this.exprQuestionRenderer = new ExprQuestionRenderer(elementFactory, widgetFactory, exprObservableFactory);
+	}
+	visitFormNode(formNode, containerElement, scope) {
+		let descriptionElement = this.elementFactory.createElement('h2');
+
+		descriptionElement.textContent = formNode.description;
+		formNode.block.accept(this, containerElement, scope);
+		containerElement.appendChild(descriptionElement);
+	}
+	visitBlockNode(blockNode, containerElement, scope) {
+		let blockScope = new Scope(scope);
+
+		for (let statement of blockNode.statements) {
+			statement.accept(this, containerElement, blockScope);
+		}
+	}
+	visitIfNode(ifNode, containerElement, scope) {
+		let ifContainerElement = this.elementFactory.createElement('div'),
+			exprObservable = this.exprObservableFactory.createExprObservable(ifNode.condition),
+			ifUpdater = new IfUpdater(ifContainerElement, exprObservable, ifNode.block, this, scope);
+
+		exprObservable.registerObserver(ifUpdater);
+		ifUpdater.notify();
+		containerElement.appendChild(ifContainerElement);
+	}
+	visitInputQuestionNode(inputQuestionNode, containerElement, scope) {
+		this.inputQuestionRenderer.render(inputQuestionNode, containerElement, scope);
+	}
+	visitExprQuestionNode(exprQuestionNode, containerElement, scope) {
+		this.exprQuestionRenderer.render(exprQuestionNode, containerElement, scope);
+	}
+}
+
+export class Renderer {
+	constructor(elementFactory, widgetFactory, exprObservableFactory) {
+		this.elementFactory = elementFactory;
+		this.widgetFactory = widgetFactory;
+		this.exprObservableFactory = exprObservableFactory;
+	}
+	render(node, containerElement) {
+		node.accept(new RenderingVisitor(this.elementFactory, this.widgetFactory, this.exprObservableFactory), containerElement, new Scope());
+	}
+}
