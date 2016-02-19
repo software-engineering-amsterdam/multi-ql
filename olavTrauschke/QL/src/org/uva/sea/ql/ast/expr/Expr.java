61fa5cead63439ffffd25d2db11b6305c9be3dc3
Olav Trauschke <olav.trauschke@student.uva.nl>
2016-02-19 11:54:27 +0100

    Added comments

--- olavTrauschke/QL/src/org/uva/sea/ql/ast/expr/Expr.java
+++ olavTrauschke/QL/src/org/uva/sea/ql/ast/expr/Expr.java
@@ -4,9 +4,11 @@ import org.uva.sea.ql.ast.ASTNode;
 
 public abstract class Expr extends ASTNode {
     
+    //force subclasses to overwrite equals
     @Override
     public abstract boolean equals(Object o);
-
+    
+    //force subclasses to overwrite hashCode
     @Override
     public abstract int hashCode();
     
