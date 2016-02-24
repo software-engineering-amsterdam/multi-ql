d12f27ec7eab9da6fb9ae0f553c2fd108ba9d20c
stevan <stevan.pavlicic@gmail.com>
2016-02-24 13:49:30 +0100

    meh...

--- stevan/src/main/java/uva/ql/ast/variables/Int.java
+++ stevan/src/main/java/uva/ql/ast/variables/Int.java
@@ -6,6 +6,8 @@ import uva.ql.interfaces.IVariable;
 
 public class Int extends AVariable {
 
+	private int value = 0;
+	
 	public Int(AST ast) {
 		super(ast);
 	}
@@ -13,6 +15,18 @@ public class Int extends AVariable {
 	@Override
 	protected int getVarType0() {
 		return IVariable.INT;
+	}
+	
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+	
+	public void setValue(String value) {
+		setValue(Integer.parseInt(value));
 	}
 
 }
