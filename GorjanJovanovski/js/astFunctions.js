235a9f733c20f673bd9253d30ef3f2a4dd12d054
Gorjan Jovanovski <jovanovski@gorjan.info>
2016-02-27 12:16:33 +0100

    Added cyclic dependency check

--- GorjanJovanovski/js/astFunctions.js
+++ GorjanJovanovski/js/astFunctions.js
@@ -92,7 +92,50 @@ function performAstChecks() {
 		}
 	);
 
+	if (noErrors) {
+		noErrors = checkDependencies();
+	}
+
 	return noErrors;
+}
+
+function checkDependencies() {
+	var map = [];
+
+	ast.transverseAST(
+		(questionNode) => {
+			if (questionNode instanceof ComputedQuestionNode) {
+				map[questionNode.label] = questionNode.computedExpr.getLabels();
+			}
+		});
+
+	var madeChange = false;
+
+	do {
+		madeChange = false;
+		for (var elementA in map) {
+			if (map.hasOwnProperty(elementA)) {
+				for (let elementB of map[elementA]) {
+					if (map[elementB] !== undefined) {
+						if (map[elementB].indexOf(elementA) !== -1) {
+							throwError(ast.getQuestion(elementA).line, "Cyclic dependencies detected between question '" + elementA + "' and question '" + elementB + "'");
+							return false;
+						}
+						for (let elementC of map[elementB]) {
+							if (map[elementA].indexOf(elementC) === -1) {
+								map[elementA].push(elementC);
+								madeChange = true;
+							}
+						}
+
+					}
+				}
+			}
+		}
+	}
+	while (madeChange);
+
+	return true;
 }
 
 function throwError(line, errorMsg) {
