07c174042ac9cf8282095aca983f6a3bb0b35d17
Spiros Tzavaras <spirostza77@hotmail.com>
2016-02-29 23:19:21 +0100

    finally

--- spiros/src/org/uva/sea/ql/type_checker/IdentifierData.java
+++ spiros/src/org/uva/sea/ql/type_checker/IdentifierData.java
@@ -1,15 +1,20 @@
 package org.uva.sea.ql.type_checker;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.uva.sea.ql.ast.type.Type;
 
 public class IdentifierData {
 
 		private final Type type;
 		private final String label;
+		private final List<String> dependencies;
 		
 		public IdentifierData(Type type, String label) {
 			this.type = type;
 			this.label = label;
+			this.dependencies = new ArrayList<String>();
 		}
 		
 		
@@ -19,5 +24,14 @@ public class IdentifierData {
 		
 		public String getLabel() {
 			return this.label;
+		}
+
+
+		public List<String> getDependencies() {
+			return dependencies;
+		}
+		
+		public void setDependencies(String dependency) {
+			this.dependencies.add(dependency);
 		}
 }
