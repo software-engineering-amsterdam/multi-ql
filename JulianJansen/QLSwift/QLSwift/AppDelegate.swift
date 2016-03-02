31fb52b910e3b4bf41bcf44eb429ab8557f91ed1
Julian Jansen <julian.jansen@gmail.com>
2016-03-02 10:43:22 +0100

    Clean project.
    Moving the prototype to a fresh project.

---
+++ JulianJansen/QLSwift/QLSwift/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  QLSwift
+//
+//  Created by Julian Jansen on 02-03-16.
+//  Copyright © 2016 Julian Jansen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
