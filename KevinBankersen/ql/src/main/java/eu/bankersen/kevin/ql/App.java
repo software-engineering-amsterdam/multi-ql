8c529465ea26ab72811a9fdae44f075910196098
Drime <k.bankersen@gmail.com>
2016-03-03 11:36:39 +0100

    Push before major refactoring..

--- KevinBankersen/ql/src/main/java/eu/bankersen/kevin/ql/App.java
+++ KevinBankersen/ql/src/main/java/eu/bankersen/kevin/ql/App.java
@@ -4,19 +4,17 @@ import java.io.IOException;
 
 import com.esotericsoftware.minlog.Log;
 import eu.bankersen.kevin.ql.ast.form.Form;
-import eu.bankersen.kevin.ql.context.Context;
 import eu.bankersen.kevin.ql.context.SymbolTable;
 import eu.bankersen.kevin.ql.context.TypeChecker;
 import eu.bankersen.kevin.ql.context.errors.ContextBuildException;
+import eu.bankersen.kevin.ql.gui.QLgui;
+import eu.bankersen.kevin.ql.interpreter.Interpreter;
 import eu.bankersen.kevin.ql.parser.ANTLRParseException;
 import eu.bankersen.kevin.ql.parser.FormParser;
 import eu.bankersen.kevin.ql.util.CustomLogger;
 import eu.bankersen.kevin.ql.util.FileReader;
 
-/**
- * Hello world!
- *
- */
+
 public final class App {
 
     private App() {
@@ -28,84 +26,55 @@ public final class App {
 
 	System.out.println("Welcome to the Questionnaire Language (QL)!\n");
 
-	String form;
-
 	String resource;
-	
+
 	//resource = "resources/Tax.form";
 	resource = "resources/Tax2.form";
 	//resource = "resources/Tax3.form";
 
-
-
+	// Read the file.
+	String file;
 	try { // Currently the top level so here we catch exceptions.
-	    form = new FileReader().read(resource);
+	    file = new FileReader().read(resource);
 	} catch (IOException e) {
 	    Log.error("Unable to read file, Terminating");
 	    System.exit(1);
-	    form = null;
+	    file = null;
 	}
 
-	FormParser parser;
-	Form parsedForm;
-
+	// Parse the form to an AST
+	Form form;
 	try {
-	    parser = new FormParser(form);
-	    parsedForm = parser.getForm();
+	    FormParser parser = new FormParser(file);
+	    form = parser.getForm();
 	    Log.info("File Parsed");
 	} catch (ANTLRParseException e) {
-	    parsedForm = null;
+	    form = null;
 	    e.getErrors().forEach(error -> Log.error("Line " + error[0] + ": " + error[1]));
 	    Log.error("Parse Errors, Terminating!");
 	    System.exit(1);
 	}
 
-	// Trying to type check.
-	Log.info("Trying to build context");
+	// Build the context object (type-checking and symbol table)
 	SymbolTable symbolTable;
 	try {
-	    symbolTable = new TypeChecker(parsedForm).getSymbolTable();
+	    symbolTable = new TypeChecker(form).getSymbolTable();
+	    Log.info("Build Context");
 	} catch (ContextBuildException e) {
 	    symbolTable = null;
 	    e.getErrors().forEach(error -> Log.error(error.toString()));
-	    Log.error("Parse Errors, Terminating!");
+	    Log.error("Context errors, Terminating!");
 	    System.exit(1);
 	}
 
+	// Create the GUI
+	QLgui ui = new QLgui(symbolTable);
+	Interpreter interp = new Interpreter(form, symbolTable);
 
-	Log.info("\n\n***********************\n"
-		+ "* Doing random  stuff *\n"
-		+ "***********************\n");
+	// Link Listeners
+	ui.addViewListener(interp);
+	interp.addDataListener(ui);
 
-	if (resource.equals("resources/Tax2.form")) {
-
-	    Log.info("Starting State");
-	    symbolTable = parsedForm.evalForm(symbolTable); // Initialize the form
-	    Log.info(parsedForm.toString());
-
-	    Log.info("Answer european: yes");
-	    symbolTable.updateSymbol("European", new Boolean(true));
-	    symbolTable = parsedForm.evalForm(symbolTable);
-	    Log.info(parsedForm.toString());
-
-	    Log.info("Answer name: Kevin");
-	    symbolTable.updateSymbol("name", new String("Kevin"));
-	    symbolTable = parsedForm.evalForm(symbolTable);
-	    Log.info(parsedForm.toString());
-
-	    Log.info("Answer age: 26");
-	    symbolTable.updateSymbol("age", new Integer(26));
-	    symbolTable = parsedForm.evalForm(symbolTable);
-	    Log.info(parsedForm.toString());
-
-	    Integer age = 4;
-	    Log.info("Answer startSchool: " + age.toString());
-	    symbolTable.updateSymbol("startSchool", age);
-	    symbolTable = parsedForm.evalForm(symbolTable);
-	    Log.info(parsedForm.toString());
-
-	    Log.info(parsedForm.toString());
-	}
-
+	ui.setVisible(true);
     }
 }
