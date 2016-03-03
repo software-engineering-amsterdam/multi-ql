f0b907e4e3c03c098e8282e841ee62538b6939ab
Julian Jansen <julian.jansen@gmail.com>
2016-03-03 20:04:48 +0100

    Beginning with expressions.

--- JulianJansen/QLSwift/QLSwift/QL/QLParser.swift
+++ JulianJansen/QLSwift/QLSwift/QL/QLParser.swift
@@ -13,28 +13,32 @@
 //
 //  form        -> form codeBlock
 //  codeBlock   -> { statement* }
-//  statement   -> question | expression
-//  question    -> stringLiteral identifier: boolLiteral
+//  statement   -> question | if ( expression ) codeBlock
+//  question    -> stringLiteral identifier: literal
 //  expression  ->
 //  literal     -> integerLiteral | floatLiteral | stringLiteral | boolLiteral
+
+// &&, ||, !, <, >, >=, <=, !=, ==, +, -, *, /.
+
+
 //
 //  Swift Reference: https://developer.apple.com/library/ios/documentation/Swift/Conceptual/
 //  Swift_Programming_Language/AboutTheLanguageReference.html
+//
+//  Parsec to Swift
+//  <^>  ->  .map
+//  >>-  ->  .flatMap
 
 import Foundation
 import SwiftParsec
 
-// Parsec to Swift
-// <^> .map
-// >>- .flatMap
-
-class QLParser: NSObject {
+class QLParser {
     
     func parseStream(data: String) throws -> QLForm {
         return try parser().run(sourceName: "", input: data)
     }
     
-    func parser() -> GenericParser<String, (), QLForm> {
+    private func parser() -> GenericParser<String, (), QLForm> {
         
         let ql = LanguageDefinition<()>.ql
         let lexer = GenericTokenParser(languageDefinition: ql)
@@ -59,8 +63,52 @@ class QLParser: NSObject {
                 
             }
         }
+        
+        // MARK: Expression.
+        
+        // Based on opTable from ExpressionTests.swift of SwiftParsec.
+        
+        // &&, ||, !, <, >, >=, <=, !=, ==, +, -, *, /.
+        
+        let opTable: OperatorTable<String, (), Int> = [
+            
+            [
+                prefix("-", function: -),
+                prefix("+", function: { $0 })
+            ],
+            [
+                postfix("++", function: { (var num) in num++ })
+            ],
+            [
+                binary(">>", function: >>, assoc: .None),
+                binary("<<", function: <<, assoc: .None)
+            ],
+            [
+                binary("*", function: *, assoc: .Left),
+                binary("/", function: /, assoc: .Left)
+            ],
+            [
+                binary("+", function: +, assoc: .Left),
+                binary("-", function: -, assoc: .Left)
+            ]
+            
+        ]
+        
+        let openingParen = StringParser.character("(")
+        let closingParen = StringParser.character(")")
+        let decimal = GenericTokenParser<()>.decimal
+        
+        let qlexpression = opTable.expressionParser { expression in
+            
+            expression.between(openingParen, closingParen) <|>
+                decimal <?> "simple expression"
+            
+            } <?> "expression"
+        
+        let qlif = symbol("if") *> qlexpression
     
         // MARK: Statement.
+        
         let qlstatement = qlquestion
         
         let qlstatements: GenericParser<String, (), [QLStatement]> = qlstatement.manyAccumulator { (let statement, var accumulated) in
@@ -86,7 +134,27 @@ class QLParser: NSObject {
         }  <?> "Error at the end of the form."
         
         return lexer.whiteSpace *> form
+    }
+    
+    // Based on functions from ExpressionTests.swift of SwiftParsec.
+    private func binary(name: String, function: (Int, Int) -> Int, assoc: Associativity) -> Operator<String, (), Int> {
+        
+        let opParser = StringParser.string(name) *> GenericParser(result: function)
+        return .Infix(opParser, assoc)
         
     }
     
+    private func prefix(name: String, function: Int -> Int) -> Operator<String, (), Int> {
+        
+        let opParser = StringParser.string(name) *> GenericParser(result: function)
+        return .Prefix(opParser)
+        
+    }
+    
+    private func postfix(name: String, function: Int -> Int) -> Operator<String, (), Int> {
+        
+        let opParser = StringParser.string(name) *> GenericParser(result: function)
+        return .Postfix(opParser.attempt)
+        
+    }
 }
