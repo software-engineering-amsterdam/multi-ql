ef92e6703c2ccb34b9dde6d8174fc57fa8549f42
NickLodewijks <NickLodewijks@gmail.com>
2016-03-03 12:26:27 +0100

    Using seperate collector visitors for collecting different kind of nodes (Questions, FreeVariables)

---
+++ NickLodewijks/src/org/uva/ql/CyclicReferences.java
@@ -0,0 +1,352 @@
+package org.uva.ql;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.uva.ql.CyclicReferences.CyclicReference;
+import org.uva.ql.CyclicReferences.ReferenceTable.Reference;
+import org.uva.ql.CyclicReferences.ReferenceTable.ReferencePath;
+import org.uva.ql.ast.form.QLQuestionnaire;
+import org.uva.ql.ast.stat.QLQuestionComputed;
+
+public class CyclicReferences implements Iterable<CyclicReference> {
+
+	private Set<CyclicReference> cyclicReferences;
+
+	private CyclicReferences() {
+		cyclicReferences = new HashSet<>();
+	}
+
+	/**
+	 * Collect all cyclic references in the questionnaire.
+	 * 
+	 * @param questionnaire
+	 *            the questionnaire to collect all cyclic references from.
+	 * @return the cyclic references in the questionnaire.
+	 */
+	public static CyclicReferences collect(QLQuestionnaire q) {
+		ComputedQuestions computedQuestions;
+		CyclicReferences cyclicReferences;
+		ReferenceTable rt;
+
+		rt = new ReferenceTable();
+
+		computedQuestions = ComputedQuestions.collect(q);
+
+		computedQuestions.forEach(question -> {
+			FreeVariables variables;
+
+			variables = FreeVariables.collect(question.expr());
+
+			rt.add(question, variables);
+		});
+
+		cyclicReferences = new CyclicReferences();
+
+		for (Reference r : rt.getReferences()) {
+			for (ReferencePath path : r.getPaths()) {
+				if (path.hasCycle()) {
+					cyclicReferences.add(new CyclicReference(r, path));
+				}
+			}
+		}
+
+		return cyclicReferences;
+	}
+
+	private void add(CyclicReference cyclicReference) {
+		cyclicReferences.add(cyclicReference);
+	}
+
+	public static class CyclicReference {
+
+		private final Reference reference;
+		private final ReferencePath path;
+
+		private CyclicReference(Reference reference, ReferencePath path) {
+			this.reference = reference;
+			this.path = path;
+		}
+
+		public Reference getReference() {
+			return reference;
+		}
+
+		public ReferencePath getPath() {
+			return path;
+		}
+	}
+
+	public static class ReferenceTable {
+
+		private final Map<String, Reference> referenceMapById;
+
+		private ReferenceTable() {
+			referenceMapById = new HashMap<>();
+		}
+
+		public void add(QLQuestionComputed question, FreeVariables variables) {
+			Reference reference;
+
+			reference = getReference(question.getId());
+
+			variables.forEach(variable -> reference.addReferent(variable));
+		}
+
+		private List<Reference> getReferences() {
+			return new ArrayList<>(referenceMapById.values());
+		}
+
+		private Reference getReference(String id) {
+			return referenceMapById.computeIfAbsent(id, f -> new Reference(id));
+		}
+
+		public static class ReferencePath {
+
+			private final List<Reference> referenceList;
+
+			public ReferencePath() {
+				referenceList = new ArrayList<>();
+			}
+
+			public ReferencePath(ReferencePath referencePath) {
+				referenceList = new ArrayList<>(referencePath.referenceList);
+			}
+
+			/**
+			 * Return a copy of this {@code ReferencePath} that is extended with
+			 * the specified reference. Extending means that the reference is
+			 * appended to the path.
+			 * 
+			 * @param r
+			 *            the {@code Reference} to add.
+			 * @return a copy of this {@code ReferencePath} with the the
+			 *         supplied reference appended.
+			 */
+			public ReferencePath extend(Reference r) {
+				ReferencePath copy;
+
+				copy = new ReferencePath(this);
+				copy.referenceList.add(r);
+
+				return copy;
+			}
+
+			public List<Reference> getPath() {
+				return Collections.unmodifiableList(referenceList);
+			}
+
+			/**
+			 * Returns the path as a string in the form of 'a -> b -> c', where
+			 * the letters are the id's of the references, and a refers to b,
+			 * and b refers to c.
+			 * 
+			 * @return the path as a string.
+			 */
+			public String getPathString() {
+				StringBuilder sb;
+
+				sb = new StringBuilder();
+
+				// Make a String in the form of 'a -> b -> c'
+				referenceList.stream().forEachOrdered(ref -> {
+					if (sb.length() > 0) {
+						sb.append(" -> ");
+					}
+					sb.append(ref.id);
+				});
+
+				return sb.toString();
+			}
+
+			/**
+			 * Returns true if this path contains the specified reference.
+			 * 
+			 * @param r
+			 *            reference whose presence in this list is to be tested
+			 * @return {@code true} if this path contains the specified
+			 *         reference
+			 */
+			public boolean contains(Reference r) {
+				return referenceList.contains(r);
+			}
+
+			/**
+			 * Returns true if this path contains a cycle.
+			 * 
+			 * @return {@code true} if this path contains a cycle, {@code false}
+			 *         otherwise.
+			 */
+			public boolean hasCycle() {
+				for (Reference r : referenceList) {
+					if (hasCycle(r)) {
+						return true;
+					}
+				}
+
+				return false;
+			}
+
+			/**
+			 * Returns true if this path contains a cycle with specified
+			 * reference.
+			 * 
+			 * @param r
+			 *            the reference to check.
+			 * @return {@code true} if this path contains a cycle with reference
+			 *         {@code r}, {@code false} otherwise.
+			 * 
+			 * @throws NullPointerException
+			 *             if {@code r} is {@code null}.
+			 */
+			public boolean hasCycle(Reference r) {
+				return frequency(r) > 1;
+			}
+
+			/**
+			 * Returns how many times the specified reference occurs in this
+			 * ReferencePath.
+			 *
+			 * @param r
+			 *            the reference whose frequency is to be determined
+			 * @return the number of elements in this path equal to {@code r}
+			 * @throws NullPointerException
+			 *             if {@code r} is null
+			 */
+			private int frequency(Reference r) {
+				return Collections.frequency(referenceList, r);
+			}
+
+			/**
+			 * Calling toString on {@code ReferencePath} will return the same
+			 * String that is returned by {@link #getPathString()}.
+			 * 
+			 * @see #getPathString()
+			 */
+			@Override
+			public String toString() {
+				return getPathString();
+			}
+		}
+
+		public class Reference {
+
+			private final String id;
+			private final List<Reference> referents = new ArrayList<>();
+
+			private Reference(String id) {
+				this.id = id;
+			}
+
+			public String getId() {
+				return id;
+			}
+
+			private void addReferent(String id) {
+				referents.add(getReference(id));
+			}
+
+			public List<Reference> getReferents() {
+				return Collections.unmodifiableList(referents);
+			}
+
+			/**
+			 * Returns all the {@code ReferencePath}'s for this
+			 * {@code References}.
+			 * 
+			 * @return the {@code ReferencesPath}'s. for this {@code Reference}.
+			 */
+			public List<ReferencePath> getPaths() {
+				return getPaths(new ReferencePath());
+			}
+
+			/**
+			 * Returns all the {@code ReferencePath}'s for this
+			 * {@code References}, appended to the specified {@code parentPath}.
+			 * 
+			 * @param parentPath
+			 *            the path to use as a root.
+			 * 
+			 * @return the {@code ReferencesPath}'s. for this {@code Reference}.
+			 */
+			public List<ReferencePath> getPaths(ReferencePath parentPath) {
+				List<ReferencePath> paths;
+				ReferencePath myPath;
+
+				myPath = parentPath.extend(this);
+
+				// We've arrived at the end of a path.
+				if (isLeaf()) {
+					return Collections.singletonList(myPath);
+				}
+
+				// Found a cycle. Continuing will cause endless recursion.
+				if (myPath.hasCycle(this)) {
+					return Collections.singletonList(myPath);
+				}
+
+				paths = new ArrayList<>();
+				for (Reference referent : referents) {
+					paths.addAll(referent.getPaths(myPath));
+				}
+
+				return Collections.unmodifiableList(paths);
+			}
+
+			/**
+			 * Returns whether this reference is a leaf. A reference is a leaf
+			 * iff the reference has no referents.
+			 * 
+			 * @return {@code true} if this reference is a leaf, {@code false}
+			 *         otherwise.
+			 */
+			private boolean isLeaf() {
+				return hasReferents();
+			}
+
+			/**
+			 * Returns whether this reference has any referents.
+			 * 
+			 * @return {@code true} if this reference has referents,
+			 *         {@code false} otherwise.
+			 */
+			private boolean hasReferents() {
+				return referents.isEmpty();
+			}
+
+			@Override
+			public final boolean equals(Object obj) {
+				Reference other;
+
+				if (!(obj instanceof Reference)) {
+					return false;
+				}
+
+				other = (Reference) obj;
+
+				return id.equals(other.id) && referents.equals(other.referents);
+			}
+
+			@Override
+			public final int hashCode() {
+				return id.hashCode();
+			}
+
+			@Override
+			public String toString() {
+				return id;
+			}
+		}
+	}
+
+	@Override
+	public Iterator<CyclicReference> iterator() {
+		return Collections.unmodifiableSet(cyclicReferences).iterator();
+	}
+}
--- NickLodewijks/src/org/uva/ql/QLSemanticAnalyser.java
+++ NickLodewijks/src/org/uva/ql/QLSemanticAnalyser.java
@@ -35,7 +35,6 @@ import org.uva.ql.ast.literal.IntegerLiteral;
 import org.uva.ql.ast.literal.StringLiteral;
 import org.uva.ql.ast.stat.QLIFStatement;
 import org.uva.ql.ast.stat.QLQuestion;
-import org.uva.ql.ast.stat.QLQuestionComputed;
 import org.uva.ql.ast.type.QLType;
 
 public class QLSemanticAnalyser {
@@ -80,7 +79,18 @@ public class QLSemanticAnalyser {
 	 * @return a {@link Result} containing errors and warnings.
 	 */
 	public Result validateCyclicReferences(QLQuestionnaire questionnaire) {
-		return new CyclicReferenceVisitor().visit(questionnaire);
+		CyclicReferences cyclicReferences;
+		Result result;
+
+		result = new Result();
+
+		cyclicReferences = CyclicReferences.collect(questionnaire);
+
+		cyclicReferences.forEach(c -> {
+			result.addError("Cyclic dependency for question %s: (%s)", c.getReference(), c.getPath());
+		});
+
+		return result;
 	}
 
 	private static class TypeCheckVisitor extends ASTNodeVisitorAdapter<QLType, SymbolTable> {
@@ -477,337 +487,12 @@ public class QLSemanticAnalyser {
 		}
 	}
 
-	public static class ReferenceTable {
-
-		private final Map<String, Reference> referenceMapById;
-
-		private ReferenceTable() {
-			referenceMapById = new HashMap<>();
-		}
-
-		/**
-		 * Find all the cyclic references
-		 * 
-		 * @return
-		 */
-		public Result findCyclicReferences() {
-			Result result;
-
-			result = new Result();
-
-			for (Reference r : referenceMapById.values()) {
-				if (r.getReferents().isEmpty()) {
-					continue;
-				}
-
-				for (ReferencePath path : r.getPaths()) {
-					if (path.hasCycle()) {
-						result.addError("Cyclic dependency for question %s: (%s)", r.getId(), path.toString());
-					}
-				}
-			}
-
-			return result;
-		}
-
-		private Reference getReference(String id) {
-			Reference r;
-
-			r = referenceMapById.get(id);
-			if (r == null) {
-				r = new Reference(id);
-				referenceMapById.put(id, r);
-			}
-
-			return r;
-		}
-
-		public static class ReferencePath {
-
-			private final List<Reference> referenceList;
-
-			public ReferencePath() {
-				referenceList = new ArrayList<>();
-			}
-
-			public ReferencePath(ReferencePath referencePath) {
-				referenceList = new ArrayList<>(referencePath.referenceList);
-			}
-
-			/**
-			 * Return a copy of this {@code ReferencePath} that is extended with
-			 * the specified reference. Extending means that the reference is
-			 * appended to the path.
-			 * 
-			 * @param r
-			 *            the {@code Reference} to add.
-			 * @return a copy of this {@code ReferencePath} with the the
-			 *         supplied reference appended.
-			 */
-			public ReferencePath extend(Reference r) {
-				ReferencePath copy;
-
-				copy = new ReferencePath(this);
-				copy.referenceList.add(r);
-
-				return copy;
-			}
-
-			public List<Reference> getPath() {
-				return Collections.unmodifiableList(referenceList);
-			}
-
-			/**
-			 * Returns the path as a string in the form of 'a -> b -> c', where
-			 * the letters are the id's of the references, and a refers to b,
-			 * and b refers to c.
-			 * 
-			 * @return the path as a string.
-			 */
-			public String getPathString() {
-				StringBuilder sb;
-
-				sb = new StringBuilder();
-
-				// Make a String in the form of 'a -> b -> c'
-				referenceList.stream().forEachOrdered(ref -> {
-					if (sb.length() > 0) {
-						sb.append(" -> ");
-					}
-					sb.append(ref.id);
-				});
-
-				return sb.toString();
-			}
-
-			/**
-			 * Returns true if this path contains the specified reference.
-			 * 
-			 * @param r
-			 *            reference whose presence in this list is to be tested
-			 * @return {@code true} if this path contains the specified
-			 *         reference
-			 */
-			public boolean contains(Reference r) {
-				return referenceList.contains(r);
-			}
-
-			/**
-			 * Returns true if this path contains a cycle.
-			 * 
-			 * @return {@code true} if this path contains a cycle, {@code false}
-			 *         otherwise.
-			 */
-			public boolean hasCycle() {
-				for (Reference r : referenceList) {
-					if (hasCycle(r)) {
-						return true;
-					}
-				}
-
-				return false;
-			}
-
-			/**
-			 * Returns true if this path contains a cycle with specified
-			 * reference.
-			 * 
-			 * @param r
-			 *            the reference to check.
-			 * @return {@code true} if this path contains a cycle with reference
-			 *         {@code r}, {@code false} otherwise.
-			 * 
-			 * @throws NullPointerException
-			 *             if {@code r} is {@code null}.
-			 */
-			public boolean hasCycle(Reference r) {
-				return frequency(r) > 1;
-			}
-
-			/**
-			 * Returns how many times the specified reference occurs in this
-			 * ReferencePath.
-			 *
-			 * @param r
-			 *            the reference whose frequency is to be determined
-			 * @return the number of elements in this path equal to {@code r}
-			 * @throws NullPointerException
-			 *             if {@code r} is null
-			 */
-			private int frequency(Reference r) {
-				return Collections.frequency(referenceList, r);
-			}
-
-			/**
-			 * Calling toString on {@code ReferencePath} will return the same
-			 * String that is returned by {@link #getPathString()}.
-			 * 
-			 * @see #getPathString()
-			 */
-			@Override
-			public String toString() {
-				return getPathString();
-			}
-		}
-
-		public class Reference {
-
-			private final String id;
-			private final List<Reference> referents = new ArrayList<>();
-
-			private Reference(String id) {
-				this.id = id;
-			}
-
-			public String getId() {
-				return id;
-			}
-
-			public void addReferent(String id) {
-				referents.add(getReference(id));
-			}
-
-			public List<Reference> getReferents() {
-				return Collections.unmodifiableList(referents);
-			}
-
-			/**
-			 * Returns all the {@code ReferencePath}'s for this
-			 * {@code References}.
-			 * 
-			 * @return the {@code ReferencesPath}'s. for this {@code Reference}.
-			 */
-			public List<ReferencePath> getPaths() {
-				return getPaths(new ReferencePath());
-			}
-
-			/**
-			 * Returns all the {@code ReferencePath}'s for this
-			 * {@code References}, appended to the specified {@code parentPath}.
-			 * 
-			 * @param parentPath
-			 *            the path to use as a root.
-			 * 
-			 * @return the {@code ReferencesPath}'s. for this {@code Reference}.
-			 */
-			public List<ReferencePath> getPaths(ReferencePath parentPath) {
-				List<ReferencePath> paths;
-				ReferencePath myPath;
-
-				myPath = parentPath.extend(this);
-
-				// We've arrived at the end of a path.
-				if (isLeaf()) {
-					return Collections.singletonList(myPath);
-				}
-
-				// Found a cycle. Continuing will cause endless recursion.
-				if (myPath.hasCycle(this)) {
-					return Collections.singletonList(myPath);
-				}
-
-				paths = new ArrayList<>();
-				for (Reference referent : referents) {
-					paths.addAll(referent.getPaths(myPath));
-				}
-
-				return Collections.unmodifiableList(paths);
-			}
-
-			/**
-			 * Returns whether this reference is a leaf. A reference is a leaf
-			 * iff the reference has no referents.
-			 * 
-			 * @return {@code true} if this reference is a leaf, {@code false}
-			 *         otherwise.
-			 */
-			private boolean isLeaf() {
-				return hasReferents();
-			}
-
-			/**
-			 * Returns whether this reference has any referents.
-			 * 
-			 * @return {@code true} if this reference has referents,
-			 *         {@code false} otherwise.
-			 */
-			private boolean hasReferents() {
-				return referents.isEmpty();
-			}
-
-			@Override
-			public final boolean equals(Object obj) {
-				Reference other;
-
-				if (!(obj instanceof Reference)) {
-					return false;
-				}
-
-				other = (Reference) obj;
-
-				return id.equals(other.id) && referents.equals(other.referents);
-			}
-
-			@Override
-			public final int hashCode() {
-				return id.hashCode();
-			}
-		}
-	}
-
-	private static class CyclicReferenceVisitor extends ASTNodeVisitorAdapter<Void, ReferenceTable> {
-
-		private QLQuestionComputed currentQuestion;
-
-		public CyclicReferenceVisitor() {
-
-		}
-
-		public Result visit(QLQuestionnaire q) {
-			ReferenceTable rt;
-
-			rt = new ReferenceTable();
-
-			q.accept(this, rt);
-
-			return rt.findCyclicReferences();
-		}
-
-		@Override
-		public Void visit(QLQuestionComputed node, ReferenceTable rt) {
-			currentQuestion = node;
-
-			node.expr().accept(this, rt);
-
-			currentQuestion = null;
-
-			return null;
-		}
-
-		@Override
-		public Void visit(VariableExpr node, ReferenceTable rt) {
-
-			// This expression is not part of the computation of a question.
-			if (currentQuestion == null) {
-				return null;
-			}
-
-			// Add the variable id of the expression as a referent of the
-			rt.getReference(currentQuestion.getId()).addReferent(node.getVariableId());
-
-			return null;
-		}
-
-	}
-
 	public static void main(String[] args) throws IOException {
 		QLQuestionnaire questionnaire;
 		QLSemanticAnalyser sa;
 		File inputFile;
 
 		inputFile = new File("resources/Questionnaire.ql");
-		// inputFile = new
-		// File("test/resources/org/uva/ql/CyclicReferences.ql");
 		questionnaire = QLQuestionnaire.create(inputFile);
 
 		sa = new QLSemanticAnalyser();
