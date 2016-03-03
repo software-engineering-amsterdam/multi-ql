cb59f1359e406cbd7d67c54945bb9ce7bfae96be
Nicasso <nico@nicasso.nl>
2016-03-03 16:29:45 +0100

    Improved the Symboltable

--- nicodegroot/src/nl/nicasso/ql/symbolTable/SymbolTable.java
+++ nicodegroot/src/nl/nicasso/ql/symbolTable/SymbolTable.java
@@ -1,9 +1,13 @@
 package nl.nicasso.ql.symbolTable;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
 
-import nl.nicasso.ql.ast.expression.Identifier;
+import nl.nicasso.ql.ast.expressions.Identifier;
+import nl.nicasso.ql.ast.types.Type;
+import nl.nicasso.ql.values.Value;
 
 public class SymbolTable {
 
@@ -24,6 +28,34 @@ public class SymbolTable {
 	
 	public SymbolTableEntry getEntry(Identifier key) {
 		return symbols.get(key);
+	}
+	
+	public Value getEntryValue(Identifier key) {
+		SymbolTableEntry entry = symbols.get(key);
+		return entry.getValue();
+	}
+	
+	public Type getEntryType(Identifier key) {
+		SymbolTableEntry entry = symbols.get(key);
+		return entry.getType();
+	}
+	
+	public void displaySymbolTable(SymbolTable symbolTable) {
+		Iterator<Entry<Identifier, SymbolTableEntry>> it = symbolTable.getSymbols().entrySet().iterator();
+	    while (it.hasNext()) {
+	    	Entry<Identifier, SymbolTableEntry> pair = it.next();
+	    	Identifier key = (Identifier) pair.getKey();
+	        SymbolTableEntry value = (SymbolTableEntry) pair.getValue();
+	        
+	        String realValue;
+	        if (value.getValue() == null) {
+	        	realValue = "undefined";
+	        } else {
+	        	realValue = value.getValue().getValue().toString();
+	        }
+	        
+	        System.out.println(key.getValue()+" ("+ value.getType().getType() +")"+ " = " + realValue);
+	    }
 	}
 	
 }
