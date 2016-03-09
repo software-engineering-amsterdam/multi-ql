c594a19b3a21f877d73caeb325e1ad3374105676
Julian Jansen <julian.jansen@gmail.com>
2016-03-09 15:07:57 +0100

    Setting up test bed for testing Visitor pattern.

--- JulianJansen/QLSwift/QLSwift/TestBed.swift
+++ JulianJansen/QLSwift/QLSwift/TestBed.swift
@@ -9,37 +9,15 @@
 import Foundation
 import SwiftParsec
 
-class TestBed {
-    
-    let symbol = GenericTokenParser(languageDefinition: LanguageDefinition<()>.ql).symbol
-    let stringLiteral = GenericTokenParser(languageDefinition: LanguageDefinition<()>.ql).stringLiteral // Includes the quotes.
-    let identifier = GenericTokenParser(languageDefinition: LanguageDefinition<()>.ql).identifier
-    let colon = GenericTokenParser(languageDefinition: LanguageDefinition<()>.ql).colon
-//    
-//    func parseStream(data: String) throws -> QLExpression {
-//        return try testExpressions().run(sourceName: "", input: data)
-//    }
-    
-//    private func testExpressions() -> GenericParser<String, (), QLExpression> {
-//        
-//
-//        
-//        
-//        
-//        
-//        
-//
-//        
-//        let openingParen = StringParser.character("(")
-//        let closingParen = StringParser.character(")")
-//        
-//        
-//
-//        
-//        
-//        return qlif
-//    }
-    
 
+/// Test setup from Lynda.com example for the visitor pattern.
+
+class House {
+    
 }
+
+class Visitor {
+    
+}
+
 
