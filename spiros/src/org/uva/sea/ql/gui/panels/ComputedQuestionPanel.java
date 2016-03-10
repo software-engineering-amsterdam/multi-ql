dbead9c242b31a1d682179dd52a00ebc93c4250a
Spiros Tzavaras <spirostza77@hotmail.com>
2016-03-10 14:15:13 +0100

    typevisitor added

--- spiros/src/org/uva/sea/ql/gui/panels/ComputedQuestionPanel.java
+++ spiros/src/org/uva/sea/ql/gui/panels/ComputedQuestionPanel.java
@@ -1,8 +1,24 @@
 package org.uva.sea.ql.gui.panels;
 
+import org.uva.sea.ql.ast.statement.ComputedQuestion;
+import org.uva.sea.ql.gui.widgets.Widget;
+
 // needed or not?
 // extends IfQuestionPanel...
 
 public class ComputedQuestionPanel extends Panel {
 
+	private ComputedQuestion question;
+	private Widget widget;
+	
+	
+	public ComputedQuestion getQuestion() {
+		return question;
+	}
+	
+	
+	public Widget getWidget() {
+		return widget;
+	}
+	
 }
