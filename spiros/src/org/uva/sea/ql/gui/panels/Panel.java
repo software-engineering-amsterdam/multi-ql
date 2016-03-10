3ad969ece975eb26469df76d2cf4ca9380009f73
Spiros Tzavaras <spirostza77@hotmail.com>
2016-03-10 22:22:19 +0100

    gui progress

--- spiros/src/org/uva/sea/ql/gui/panels/Panel.java
+++ spiros/src/org/uva/sea/ql/gui/panels/Panel.java
@@ -6,22 +6,22 @@ import javax.swing.JPanel;
 
 //extend Component or .getPanel?
 
-public abstract class Panel {
+public abstract class Panel extends JPanel {
 
-	private JPanel panel;
-	
-	public Panel() {
-		this.panel = new JPanel();
-		panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
-
-	}
-
-	public JPanel getPanel() {
-		return panel;
-	}
-
-	public void addPanel(Panel argPanel) {
-		panel.add(argPanel.getPanel());
-	}
+//	private JPanel panel;
+//	
+//	public Panel() {
+//		this.panel = new JPanel();
+//		panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+//
+//	}
+//
+//	public JPanel getPanel() {
+//		return panel;
+//	}
+//
+//	public void addPanel(Panel argPanel) {
+//		panel.add(argPanel.getPanel());
+//	}
 	
 }
--- spiros/src/org/uva/sea/ql/gui/widgets/Widget.java
+++ spiros/src/org/uva/sea/ql/gui/widgets/Widget.java
@@ -1,5 +1,15 @@
 package org.uva.sea.ql.gui.widgets;
 
-public abstract class Widget {
+public abstract class Widget<T> {
+	
+	private final T widget;
+	
+	public Widget(T value) {
+		this.widget = value;
+	}
+	
+	public T getWidget() {
+		return this.widget;
+	}
 
 }
