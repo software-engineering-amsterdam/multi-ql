7c82218a5ebaa9b2dbad4a3a0f28b30abe51f6ce
Tom van Duist <tomvanduist@gmail.com>
2016-03-16 13:21:29 +0100

    Revert "Restructure"
    This reverts commit 100b7736e9fdb43dfccac05efc4c2608d939293b.

--- tomvanduist/QLSwift/Pods/Target Support Files/Pods-QLSwift/Pods-QLSwift-frameworks.sh
+++ tomvanduist/QLSwift/Pods/Target Support Files/Pods-QLSwift/Pods-QLSwift-frameworks.sh
@@ -86,10 +86,8 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "Pods-QLSwift/DTAlertView.framework"
   install_framework "Pods-QLSwift/SnapKit.framework"
-  install_framework "Pods-QLSwift/SwiftParsec.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "Pods-QLSwift/DTAlertView.framework"
   install_framework "Pods-QLSwift/SnapKit.framework"
-  install_framework "Pods-QLSwift/SwiftParsec.framework"
 fi
