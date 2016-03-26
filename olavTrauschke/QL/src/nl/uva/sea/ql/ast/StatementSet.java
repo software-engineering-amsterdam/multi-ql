d238a702f072de2fc38dc2f450844c0e604a2ca2
Olav Trauschke <olav.trauschke@student.uva.nl>
2016-03-26 19:42:00 +0100

    Documentation

--- olavTrauschke/QL/src/nl/uva/sea/ql/ast/StatementSet.java
+++ olavTrauschke/QL/src/nl/uva/sea/ql/ast/StatementSet.java
@@ -45,7 +45,7 @@ public class StatementSet extends ASTNode {
      * <code>visitor visit this StatementSet</code>. The <code>ASTNode</code>s are
      * visited in reverse order, to process them in the same order as they were
      * in a ql-file that was analyzed by a
-     * {@link nl.uva.sea.ql.parser.ParserWrapper Parser(Wrapper)}
+     * {@link nl.uva.sea.ql.parser.ParserWrapper Parser(Wrapper)}.
      * 
      * @param visitor an <code>ASTVisitor</code> that should
      *          <code>visit this StatementSet</code> and its children
