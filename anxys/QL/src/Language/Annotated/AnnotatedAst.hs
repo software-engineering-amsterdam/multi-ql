a555a1fef0d013dd9b5d649d5f1764ee2e6ce960
James Bartholomew <anxys@hotmail.co.uk>
2016-03-28 20:24:30 +0200

    Added helper functions to asts and made them more generic

--- anxys/QL/src/Language/Annotated/AnnotatedAst.hs
+++ anxys/QL/src/Language/Annotated/AnnotatedAst.hs
@@ -1,8 +1,10 @@
 module AnnotatedAst where
 
+import Data.Decimal
+import Prelude hiding (id)
 import Identifier
 --Todo use special datatype for this. Or at the very least don't do arithmethic with it
-type Money = Double
+type Money = Decimal
 
 
 type Block a = [Statement a]
@@ -62,3 +64,49 @@ data FieldInformation a =
 
 data Form a = Form a String (Block a)
   deriving Show
+
+
+collectFields :: Form a -> [Field a]
+collectFields (Form _ _ stmnts) =
+  getFieldStmnts stmnts
+  where
+    getFieldStmnts =
+      concatMap getFieldStmnts'
+    getFieldStmnts' (If _ _ ifblock) =
+      getFieldStmnts ifblock
+    getFieldStmnts' (IfElse _ _ ifblock elseblock) =
+      getFieldStmnts ifblock ++ getFieldStmnts elseblock
+    getFieldStmnts' (Field _ field) =
+      [field]
+
+getIdentifier :: Field a -> Identifier
+getIdentifier (CalculatedField _ info _) = id info
+getIdentifier (SimpleField _ info) = id info
+
+collectFieldInformation :: Form a -> [FieldInformation a]
+collectFieldInformation (Form _ _ stmts) =
+  getFieldInfo stmts
+  where
+    getFieldInfo = concatMap getFieldInfo'
+    getFieldInfo' st =
+      case st of
+        Field _ (SimpleField _ f) ->
+          [f]
+        Field _ (CalculatedField _ f _) ->
+          [f]
+        If _ _ ss ->
+          getFieldInfo ss
+        IfElse _ _ xs ys ->
+          getFieldInfo xs ++ getFieldInfo ys
+
+extractAnnotationFromField :: Field a -> a
+extractAnnotationFromField (CalculatedField anno _ _) = anno
+extractAnnotationFromField (SimpleField anno _) = anno
+
+collectCalculatedFields :: Form a -> [Field a]
+collectCalculatedFields form = filter isCalc (collectFields form)
+  where
+    isCalc x =
+      case x of
+        CalculatedField{} -> True
+        SimpleField{}     -> False
