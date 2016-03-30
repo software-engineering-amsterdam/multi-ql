2079eacb75d9e68aa387f1bb5b92e1c14722a5b0
James Bartholomew <anxys@hotmail.co.uk>
2016-03-30 13:37:37 +0200

    Added money and helper functions to find the expressions that fields depend on

--- anxys/QL/src/Language/Annotated/AnnotatedAst.hs
+++ anxys/QL/src/Language/Annotated/AnnotatedAst.hs
@@ -1,11 +1,16 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
 module AnnotatedAst where
 
 import Data.Decimal
+import Data.Generics.Uniplate.Data
 import Prelude hiding (id)
+import Data.Typeable
+import Data.Data
+import Money
 import Identifier
-
-type Money = Decimal
-
 
 type Block a = [Statement a]
 
@@ -13,22 +18,22 @@ data FieldType a = Money a
                  | Integer a
                  | String a
                  | Boolean a
-  deriving (Eq, Show)
+  deriving (Eq, Show, Typeable, Data)
 
 data Literal a = IntegerLiteral a Integer
                | MoneyLiteral a Money
                | StringLiteral a String
                | BooleanLiteral a Bool
-  deriving (Eq, Show)
+  deriving (Eq, Show, Typeable, Data)
 
 data Expression a = Variable a Identifier
                   | Literal a (Literal a)
                   | BinaryOperation a (BinaryOperation a) (Expression a) (Expression a)
                   | UnaryOperation a (UnaryOperation a) (Expression a)
-  deriving (Eq, Show)
+  deriving (Eq, Show, Typeable, Data)
 
 data UnaryOperation a = Not a
-  deriving (Eq, Show)
+  deriving (Eq, Show, Typeable, Data)
 
 data BinaryOperation a = Addition a
                        | Subtraction a
@@ -43,16 +48,16 @@ data BinaryOperation a = Addition a
                        | GreaterThanOrEquals a
                        | LesserThan a
                        | LesserThanOrEquals a
-  deriving (Eq, Show)
+  deriving (Eq, Show, Typeable, Data)
 
 data Statement a = Field a (Field a)
                  | If a (Expression a) (Block a)
                  | IfElse a (Expression a) (Block a) (Block a)
-  deriving (Eq, Show)
+  deriving (Eq, Show, Typeable, Data)
 
 data Field a = SimpleField a (FieldInformation a)
              | CalculatedField a (FieldInformation a) (Expression a)
-  deriving (Eq, Show)
+  deriving (Eq, Show, Typeable, Data)
 
 data FieldInformation a =
        FieldInformation
@@ -60,50 +65,35 @@ data FieldInformation a =
          , id :: Identifier
          , fieldType :: FieldType a
          }
-  deriving (Eq, Show)
+  deriving (Eq, Show, Typeable, Data)
 
 data Form a = Form a String (Block a)
-  deriving Show
+  deriving (Eq, Show, Typeable, Data)
 
 
-collectFields :: Form a -> [Field a]
-collectFields (Form _ _ stmnts) =
-  getFieldStmnts stmnts
-  where
-    getFieldStmnts =
-      concatMap getFieldStmnts'
-    getFieldStmnts' (If _ _ ifblock) =
-      getFieldStmnts ifblock
-    getFieldStmnts' (IfElse _ _ ifblock elseblock) =
-      getFieldStmnts ifblock ++ getFieldStmnts elseblock
-    getFieldStmnts' (Field _ field) =
-      [field]
+collectFields :: Data a => Form a -> [Field a]
+collectFields (Form _ _ stmnts) = concatMap fields' stmnts
+  where fields' x = [y | Field _  y <- universe x]
 
 getIdentifier :: Field a -> Identifier
 getIdentifier (CalculatedField _ info _) = id info
 getIdentifier (SimpleField _ info) = id info
 
-collectFieldInformation :: Form a -> [FieldInformation a]
-collectFieldInformation (Form _ _ stmts) =
-  getFieldInfo stmts
+collectFieldInformation :: Data a => Form a -> [FieldInformation a]
+collectFieldInformation form = map getFieldInfo (collectFields form)
   where
-    getFieldInfo = concatMap getFieldInfo'
-    getFieldInfo' st =
-      case st of
-        Field _ (SimpleField _ f) ->
-          [f]
-        Field _ (CalculatedField _ f _) ->
-          [f]
-        If _ _ ss ->
-          getFieldInfo ss
-        IfElse _ _ xs ys ->
-          getFieldInfo xs ++ getFieldInfo ys
+    getFieldInfo (SimpleField _ f) = f
+    getFieldInfo (CalculatedField _ f _) = f
+
+extractFieldInfo :: Field a -> FieldInformation a
+extractFieldInfo (CalculatedField _ i _) = i
+extractFieldInfo (SimpleField _ i) = i
 
 extractAnnotationFromField :: Field a -> a
 extractAnnotationFromField (CalculatedField anno _ _) = anno
 extractAnnotationFromField (SimpleField anno _) = anno
 
-collectCalculatedFields :: Form a -> [Field a]
+collectCalculatedFields :: Data a => Form a -> [Field a]
 collectCalculatedFields form = filter isCalc (collectFields form)
   where
     isCalc x =
